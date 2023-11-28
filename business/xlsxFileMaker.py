8134712e0ff2575e5ed6232f0b5ec280095acfea
dptzy <peerapat10322@gmail.com>
2023-11-28 08:58:40 +0700

    28/11

---
+++ business/xlsxFileMaker.py
@@ -0,0 +1,11 @@
+import datetime
+import openpyxl
+
+class XlsxFileMaker : 
+    def __init__(self) :
+        self.fileName = ""
+        pass 
+
+    def createFile(self) : 
+        self.fileName = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") 
+        pass 
